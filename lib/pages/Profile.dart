b05be2175cc0ffe4e71cdd66524f9ee09217e184
belvanaufal <qhikizaalicia@gmail.com>
2024-11-04 19:18:03 +0700

    nambahin foto di Profile

--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -42,7 +42,7 @@ class ProfilePage extends StatelessWidget {
             children: [
               CircleAvatar(
                 radius: 50,
-                backgroundImage: AssetImage('assets/profile.jpg'),
+                backgroundImage: AssetImage('assets/img/profile.jpg'),
               ),
               SizedBox(height: 15),
               Text(
