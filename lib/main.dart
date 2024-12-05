a47536b202a831d932d671923f30858af1318711
fauzein <fauzeinmw2@gmail.com>
2024-12-05 22:52:04 +0700

    p

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:simpkl_mobile/components/BottomNavBar.dart';
 import 'package:simpkl_mobile/pages/home_page.dart';
 import 'package:simpkl_mobile/pages/jurnal_page.dart';
